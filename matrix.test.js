39ce54e0225d4a364d7a2a97d780f305d47f3a63
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-01-27 20:12:55 +0545

    changed expect value in test cases for testing

--- matrix.test.js
+++ matrix.test.js
@@ -1,20 +1,18 @@
-
-import Matrix from './matrix.js';
-import { expect } from 'jest';
+const Matrix = require('./matrix.js');
 
 test('add scalar to matrix', () => {
   let m = new Matrix(3, 3);
   m.data[0] = [1, 2, 3];
   m.data[1] = [4, 5, 6];
   m.data[2] = [7, 8, 9];
-  m.add(2);
+  m.add(1);
   expect(m).toEqual({
     rows: 3,
     cols: 3,
     data: [
-      [3, 4, 5],
-      [6, 7, 8],
-      [9, 10, 11],
+      [2, 3, 4],
+      [5, 6, 7],
+      [8, 9, 10],
     ],
   });
 });
